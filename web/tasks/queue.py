bf322e5acd74e6e13ae4034a1a3c8ca588382792
avkosme <akostyuchenko@oneclickmoney.ru>
2018-09-12 15:05:25 +0300

    Поменял изменение значение счетчика (counter) на get_or_create (update)

--- web/tasks/queue.py
+++ web/tasks/queue.py
@@ -1,5 +1,6 @@
 import os
 import django
+from django.db.models import F
 from celery import Celery, shared_task
 
 os.environ.setdefault(
@@ -20,14 +21,14 @@ app.config_from_object(
 def counter(**kwargs):
     from content.models import Video, Audio, Text
     if kwargs.get('type') == 'video':
-        video = Video.objects.get(pk=kwargs.get('pk'))
-        video.counter = video.counter + 1
+        video, pk = Video.objects.get_or_create(pk=kwargs.get('pk'))
+        video.counter = F('counter') + 1
         return video.save()
     if kwargs.get('type') == 'audio':
-        audio = Audio.objects.get(pk=kwargs.get('pk'))
-        audio.counter = audio.counter + 1
+        audio, pk = Audio.objects.get_or_create(pk=kwargs.get('pk'))
+        audio.counter = F('counter') + 1
         return audio.save()
     if kwargs.get('type') == 'text':
-        text = Text.objects.get(pk=kwargs.get('pk'))
-        text.counter = text.counter + 1
+        text, pk = Text.objects.get_or_create(pk=kwargs.get('pk'))
+        text.counter = F('counter') + 1
         return text.save()
